f402848d0e0b2c5a5fad9b8717df4e8f49936a7d
buddhisha51 <buddeeshachamaka512@gmail.com>
2026-04-29 22:55:28 +0530

    phase 4 end commit

--- Netmiko_tasks/check_connection.py
+++ Netmiko_tasks/check_connection.py
@@ -2,10 +2,10 @@ from netmiko import ConnectHandler
 
 # 1. Define the device details (The "Target")
 Device = {
-    'device_type': 'aruba_aoscx',
-    'host': '192.168.2.233',
-    'username': 'admin', 
-    'password': 'admin@123',
+    'device_type': 'cisco_ios',
+    'host': '10.10.20.48',
+    'username': 'developer', 
+    'password': 'C1sco12345',
     'port': 22,
 }
 
@@ -15,6 +15,7 @@ print("Connecting to the switch... please wait.")
 connection = ConnectHandler(**Device)
 
 print("Success! Connection established.")
+print("connected")
 
 # 5. Close the connection
 connection.disconnect()
